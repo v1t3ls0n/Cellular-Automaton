aa2abb699f72f483a842b8852fff54df03ff6632
v1t3ls0n <guyvitelson@gmail.com>
2024-11-17 22:29:56 +0200

    update to include 3D graphics as well and initilized grid corresponding to the real world

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 from core import Simulation
-from display import Display,MatplotlibDisplay
+from display import MatplotlibDisplay, GraphDisplay
 
 # יצירת סימולציה
 sim = Simulation(grid_size=10, days=365)
 sim.run()
 
-# תצוגת Tkinter (דו-ממדית)
-display = Display(sim)
-display.run()
-
 # תצוגת Matplotlib (תלת-ממדית)
 matplotlib_display = MatplotlibDisplay(sim)
 matplotlib_display.plot_3d()
+
+metrics = GraphDisplay(sim)
+metrics.plot_average_pollution()
