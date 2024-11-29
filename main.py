42dfbd082aa23b5527b7df32b337d74e91148208
v1t3ls0n <guyvitelson@gmail.com>
2024-11-29 04:39:17 +0200

    work

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ import logging
 
 # Set up logging
 logging.basicConfig(
-    filename="simulation.log",
-    level=logging.INFO,
-    format="%(asctime)s - %(levelname)s - %(message)s",
-    datefmt="%Y-%m-%d %H:%M:%S",
+    # filename="simulation.log",
+    # level=logging.INFO,
+    # format="%(asctime)s - %(levelname)s - %(message)s",
+    # datefmt="%Y-%m-%d %H:%M:%S",
 )
 logging.info("Simulation started.")
 
