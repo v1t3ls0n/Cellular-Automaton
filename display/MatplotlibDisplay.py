caf519c3ef3d67a0873fccff164fcbaa0ad23494
v1t3ls0n <guyvitelson@gmail.com>
2024-12-02 04:29:57 +0200

    best layout

--- display/MatplotlibDisplay.py
+++ display/MatplotlibDisplay.py
@@ -95,14 +95,14 @@ class MatplotlibDisplay:
         scrollable_canvas.bind_all("<MouseWheel>", _on_mouse_wheel)
 
         # Create a Matplotlib figure
-        fig = plt.Figure(figsize=(18, 54), tight_layout=True)  # Adjust figure size
+        fig = plt.Figure(figsize=(18,36), tight_layout=True)  # Adjust figure size
         # 7 rows for standardized and non-standardized graphs
         gs = fig.add_gridspec(7, 2)  # 7 rows for standardized and non-standardized graphs
         fig.subplots_adjust(hspace=0.2)  # Adjust spacing between plots
 
         self.fig = fig
         self.canvas = FigureCanvasTkAgg(self.fig, master=scrollable_frame)
-        self.canvas.get_tk_widget().pack(expand=True)
+        self.canvas.get_tk_widget().pack(fill="both",padx=4)
 
         # Create subplots
         self.axes = {
