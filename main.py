53fcc493f77403f833a3a96a78c0ea10dbe88ec9
v1t3ls0n <guyvitelson@gmail.com>
2024-12-04 09:51:41 +0200

    update

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ console_handler = logging.StreamHandler()
 console_handler.setLevel(logging.INFO)
 
 # Formatter
-formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+formatter = logging.Formatter("%(levelname)s - %(message)s")
 file_handler.setFormatter(formatter)
 console_handler.setFormatter(formatter)
 
